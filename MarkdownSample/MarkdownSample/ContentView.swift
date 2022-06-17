c8b50961fc3c7a0b7b982c2295e9a089cc298517
kotetuco <paprapapra@gmail.com>
2022-06-17 09:27:57 +0900

    Add project.

---
+++ MarkdownSample/MarkdownSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MarkdownSample
+//
+//  Created by kotetu on 2022/06/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MarkdownSample/MarkdownSample/MarkdownSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarkdownSampleApp.swift
+//  MarkdownSample
+//
+//  Created by kotetu on 2022/06/17.
+//
+
+import SwiftUI
+
+@main
+struct MarkdownSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
