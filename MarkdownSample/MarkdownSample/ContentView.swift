2b71b2bb9a19e8f725a6bac5758ca82980163dc5
kotetuco <paprapapra@gmail.com>
2022-06-20 12:55:06 +0900

    Add Text (using several Markdown format)

--- MarkdownSample/MarkdownSample/ContentView.swift
+++ MarkdownSample/MarkdownSample/ContentView.swift
@@ -9,8 +9,45 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ScrollView {
+            VStack {
+                VStack {
+                    // 対応してるフォーマット
+                    Text("Hello, world!")
+                        .padding()
+                    Text("*Hello, world!*")
+                        .padding()
+                    Text("**Hello, world!**")
+                        .padding()
+                    Text("***Hello, world!***")
+                        .padding()
+                    Text("~Hello, world!~")
+                        .padding()
+                    Text("[Hello, world!](https://ja.wikipedia.org/wiki/Hello_world)")
+                        .padding()
+                    Text("`Hello, world!`")
+                        .padding()
+                }
+                VStack {
+                    // 対応してないフォーマット
+                    Text("# Hello, world!")
+                        .padding()
+                    Text("## Hello, world!")
+                        .padding()
+                    Text("- Hello, world!")
+                        .padding()
+                    Text("* Hello, world!")
+                        .padding()
+                    Text("> Hello, world!")
+                        .padding()
+                    Text("- [x] Hello, world!")
+                        .padding()
+                    Text("---")
+                        .padding()
+                }
+            }
+            .frame(maxWidth: .infinity)
+        }
     }
 }
 
