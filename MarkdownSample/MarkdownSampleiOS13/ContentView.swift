0a1a715f1b428826408b1cdee3be4582d3e6fbb6
kotetuco <paprapapra@gmail.com>
2022-07-30 19:01:03 +0900

    Fix comments and additional views.

--- MarkdownSample/MarkdownSampleiOS13/ContentView.swift
+++ MarkdownSample/MarkdownSampleiOS13/ContentView.swift
@@ -8,41 +8,66 @@
 import SwiftUI
 
 struct ContentView: View {
+    private let codeBlock = """
+```swift
+let text = "Hello, world!"
+```
+"""
+
     var body: some View {
         ScrollView {
             VStack {
                 VStack {
-                    // 対応してるフォーマット
-                    Text("Hello, world!")
+                    // 利用可能な構文
+                    // 太字 + ボールド　+ 打ち消し線
+                    Text("~***Hello, world!***~")
                         .padding()
+
+                    //
+                    // Label構造体はiOS 13では利用できない
+                    // 
+
+                    // イタリック
                     Text("*Hello, world!*")
                         .padding()
+                    // イタリック
+                    Text("_Hello, world!_")
+                        .padding()
+                    // ボールド
                     Text("**Hello, world!**")
                         .padding()
-                    Text("***Hello, world!***")
-                        .padding()
+                    // 打ち消し線
                     Text("~Hello, world!~")
                         .padding()
-                    Text("[Hello, world!](https://ja.wikipedia.org/wiki/Hello_world)")
+                    // リンク
+                    Text("[Link](https://ja.wikipedia.org/wiki/Hello_world)")
                         .padding()
+                    // インラインコード
                     Text("`Hello, world!`")
                         .padding()
                 }
                 VStack {
-                    // 対応してないフォーマット
+                    // 対応してない構文
+                    // 見出し
                     Text("# Hello, world!")
                         .padding()
+                    // 見出し
                     Text("## Hello, world!")
                         .padding()
+                    // 箇条書き
                     Text("- Hello, world!")
                         .padding()
+                    // 箇条書き
                     Text("* Hello, world!")
                         .padding()
+                    // 引用
                     Text("> Hello, world!")
                         .padding()
-                    Text("- [x] Hello, world!")
-                        .padding()
+                    // 水平線
                     Text("---")
+                        .padding()
+                    // コードブロック
+                    Text(codeBlock)
                         .padding()
                 }
             }
